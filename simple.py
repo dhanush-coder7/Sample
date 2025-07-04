c8bce4798132c7b5d795b59ac39a797bbd4c5282
dhanush-coder7 <93364879+dhanush-coder7@users.noreply.github.com>
2025-07-04 12:02:20 +0530

    Update 2 simple.py

--- simple.py
+++ simple.py
@@ -1,1 +1,9 @@
-print("Jenkins says hello!")
+# simple.py
+def generate_file():
+    with open("build_output/result.txt", "w") as f:
+        f.write("This is a generated file from Jenkins build process.\n")
+
+if __name__ == "__main__":
+    print("Starting build process...")
+    generate_file()
+    print("Build process completed.")
